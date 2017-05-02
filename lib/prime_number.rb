b73dca3e98c49b560821f60b8610271e2bcd19e5
Deniss <den.b@itservicesgroup.co.uk>
2017-05-02 17:06:28 +0100

    Add Spec list prime numbers.

--- lib/prime_number.rb
+++ lib/prime_number.rb
@@ -1,7 +1,15 @@
 class PrimeNumber
 
  def self.take(number)
-   [*1..number]
+   max = [*2..number]
+   max.each_with_object([]) do |item, array|
+     for i in (2..item) do
+       break if item % i == 0
+     end
+     array << i if i == item
+     max << item += 1 if item >= number
+     return array if array.size == number
+   end
  end
 
 end
--- spec/prime_number_spec.rb
+++ spec/prime_number_spec.rb
@@ -7,5 +7,10 @@ RSpec.describe PrimeNumber do
      expect(output.size).to eq 10
    end
 
+   it "should list prime numbers" do
+     output = PrimeNumber.take(10)
+     expected = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+     expect(output).to eq(expected)
+   end
  end
 end
